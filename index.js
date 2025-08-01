7a6f570aadb609bb3170651c18fa9c70e9f8f89b
Sarvesh Mondkar <sarveshmondkar3084@gmail.com>
2025-08-01 18:45:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
